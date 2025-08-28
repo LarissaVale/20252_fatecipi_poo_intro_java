ca506e5fd98188b1b9d62cb1d1def77a7aa26d28
larissa vitoria nogueira do vale <larissavitorianogueira13@gmail.com>
2025-08-28 15:54:02 -0300

    learn(ap002-java): encerra apostila

---
+++ IOLinhaDeComando.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IOLinhaDeComando{
+    public static void main(String... args){
+        //declaração de variáveis
+        int a, b;
+        int resultado;
+        Scanner leitor = new Scanner(System.in);
+        //entrada
+        System.out.println("digite o primeiro valor");
+        a = leitor.nextInt();
+        System.out.println("digite o proximo valor");
+        b = leitor.nextInt();
+        //processamento
+        resultado = a + b;
+        //saída
+        //2 + 3 = 5
+        System.out.printf("%d + %d = %d\n",a, b, resultado);
+    }
+}
